770a4355785f050bb7b2a358d14d4bd2b7693304
Sean Liao <seankhliao@gmail.com>
2019-03-02 02:57:42 +0800

    new service worker

--- src/sw.js
+++ src/sw.js
@@ -12,25 +12,35 @@ this.addEventListener("install", event => {
 });
 
 self.addEventListener("fetch", e => {
+  let cached = false;
   e.respondWith(
-    caches.open(cacheName).then(cache => {
-      cache.match(e.request).then(response => {
-        console.log(response);
-        return (
-          response ||
-          fetch(e.request).then(response => {
-            console.log(response);
-            return response;
-          })
-        );
+    caches.match(e.request).then(res => {
+      if (res) {
+        cache = true;
+        return res;
+      }
+      return fetch(e.request).then(res => {
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+        let res2cache = res.clone();
+        caches.open(cacheName).then(cache => {
+          cache.put(e.request, res2cache);
+        });
+        return res;
       });
     })
   );
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      fetch(e.request).then(response => {
-        cache.put(e.request, response);
-      });
-    })
-  );
+  if (cached) {
+    e.waitUntil(
+      fetch(e.request).then(res => {
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return;
+        }
+        caches.open(cacheName).then(cache => {
+          cache.put(e.request, res);
+        });
+      })
+    );
+  }
 });
