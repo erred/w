bf86e13c7578676584341db003d679836b1bd3df
Sean Liao <seankhliao@gmail.com>
2019-05-27 19:47:48 +0800

    add site builder

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"io"
 	"io/ioutil"
 	"log"
@@ -16,217 +17,234 @@ import (
 )
 
 var (
-	src = "src"
-	dst = "dst"
-	ext = map[string]string{
-		".md": ".html",
-	}
+	IgnoreExt = make(map[string]struct{})
+	Src, Dst  string
+	TmplExt   string
+	MdExt     = ".md"
 )
 
+func init() {
+	var ignoreExt string
+	flag.StringVar(&ignoreExt, "ignoreext", ".ico,.svg,.png,.jpg", "comma separated list of extensions to ignore")
+	flag.StringVar(&Src, "src", "src", "source directory")
+	flag.StringVar(&Dst, "dst", "dst", "output directory")
+	// name templates:
+	// $basedir-post :ex /src/bog/template.gohtml => "blog-post"
+	flag.StringVar(&TmplExt, "tmplext", ".gohtml", "extension for html templates")
+
+	flag.Parse()
+	for _, e := range strings.Split(ignoreExt, ",") {
+		IgnoreExt[e] = struct{}{}
+	}
+}
+
 func main() {
-	w := NewW()
-	w.Start()
+	p := NewProcessor()
+	p.Walk()
+	p.Process()
 }
 
-type W struct {
-	wg    sync.WaitGroup
-	t     *template.Template
-	posts chan Post
+type Processor struct {
+	t             *template.Template
+	templatePaths []string
+	copyPaths     []string
+	mdPaths       map[string][]string
 }
 
-func NewW() *W {
-	t := template.Must(template.New("head").Parse(HeadTemplate))
-	t = template.Must(t.New("foot").Parse(FootTemplate))
-	t = template.Must(t.New("post").Parse(PostTemplate))
-	t = template.Must(t.New("index").Parse(IndexTemplate))
-
-	return &W{
-		posts: make(chan Post, 32),
-		t:     t,
+func NewProcessor() *Processor {
+	return &Processor{
+		mdPaths: make(map[string][]string),
 	}
 }
 
-func (w *W) Start() {
-	go func() {
-		filepath.Walk(src, w.walker)
-		w.wg.Wait()
-		close(w.posts)
-	}()
-	w.writeIndex()
+func (p *Processor) Walk() {
+	filepath.Walk(Src, p.walker)
 }
+func (p *Processor) Process() error {
+	var wg sync.WaitGroup
+	var err error
 
-func (w *W) walker(path string, info os.FileInfo, err error) error {
-	if err != nil || info.IsDir() {
-		return nil
+	p.t, err = template.ParseFiles(p.templatePaths...)
+	if err != nil {
+		log.Printf("Process parseFiles %v\n", err)
+		return err
 	}
-	switch filepath.Ext(path) {
-	case ".md":
-		w.Convert(path)
-	default:
-		w.Copy(path)
+
+	defer wg.Wait()
+
+	for _, fp := range p.copyPaths {
+		wg.Add(1)
+		go func(fp string) {
+			defer wg.Done()
+			p.copyFile(fp)
+		}(fp)
 	}
+
+	for dir, fps := range p.mdPaths {
+		wg.Add(1)
+		go func(dir string, fps []string) {
+			defer wg.Done()
+			p.md2html(dir, fps)
+		}(dir, fps)
+	}
+
 	return nil
 }
 
-func (w *W) writeIndex() {
-	var ps []Post
-	for p := range w.posts {
-		ps = append(ps, p)
-	}
-	sort.Sort(Posts(ps))
-
-	nfn := filepath.Join(dst, "blog/index.html")
-	d, err := create(nfn)
+func (p *Processor) walker(fp string, info os.FileInfo, err error) error {
 	if err != nil {
-		log.Printf("Copy error opening %v: %v\n", nfn, err)
+		log.Printf("walker called with %v\n", err)
+		return nil
+	}
+	if info.IsDir() {
+		return nil
+	}
+
+	ext := filepath.Ext(fp)
+	if _, ok := IgnoreExt[ext]; ok {
+		// noop
+	} else if ext == TmplExt {
+		p.templatePaths = append(p.templatePaths, fp)
+	} else if ext == MdExt {
+		dir := filepath.Dir(fp)
+		p.mdPaths[dir] = append(p.mdPaths[dir], fp)
+	} else {
+		p.copyPaths = append(p.copyPaths, fp)
+	}
+
+	return nil
+}
+
+func (p *Processor) md2html(dir string, fps []string) {
+	var posts []Post
+	bd := filepath.Base(dir)
+	posttmpl := bd + "-post"
+	idxtmpl := bd + "-index"
+
+	for _, fp := range fps {
+		posts = append(posts, p.parsePost(posttmpl, fp))
+	}
+
+	sort.Sort(Posts(posts))
+
+	idx := map[string]interface{}{
+		"Posts": posts,
+		"Title": bd,
+		"URL":   strings.TrimPrefix(dir, Src),
+	}
+
+	nfn := filepath.Join(p.src2dst(dir), "index.html")
+	f, err := p.newFile(nfn)
+	if err != nil {
+		log.Printf("md2html newFile %v: %v\n", nfn, err)
 		return
 	}
-	defer d.Close()
+	defer f.Close()
 
-	idx := Index{Posts: ps, Description: "blog of seankhliao"}
-
-	err = w.t.ExecuteTemplate(d, "index", idx)
+	err = p.t.ExecuteTemplate(f, idxtmpl, idx)
 	if err != nil {
-		log.Printf("error executing template %v for %v: %v\n", "index", nfn, err)
+		log.Printf("md2html exec template %v for %v: %v", idxtmpl, nfn, err)
 		return
+	}
+
+}
+
+func (p *Processor) parsePost(tmpl, fp string) Post {
+	var pt Post
+
+	b, err := ioutil.ReadFile(fp)
+	if err != nil {
+		log.Printf("parsePost readfile %v: %v\n", fp, err)
+		return pt
+	}
+
+	pt.URL = strings.TrimPrefix(strings.TrimSuffix(fp, MdExt), Src)
+
+	bb := bytes.SplitN(b, []byte("\n---\n"), 2)
+	if len(bb) != 2 {
+		log.Printf("parsePost split expected --- in %v\n", fp)
+		return pt
+	}
+	for ln, line := range bytes.Split(bytes.TrimSpace(bb[0]), []byte("\n")) {
+		l := bytes.SplitN(line, []byte("="), 2)
+		if len(l) != 2 {
+			log.Printf("parsePost parse header %v line %v expected split by = \n", fp, ln)
+			return pt
+		}
+		v := string(bytes.TrimSpace(l[1]))
+		switch string(bytes.TrimSpace(l[0])) {
+		case "title":
+			pt.Title = v
+		case "date":
+			pt.Date = v
+		case "desc", "description":
+			pt.Desc = v
+		default:
+			log.Printf("parsePost parse header %v line %v unkown kv: %v\n", fp, ln, l)
+		}
+	}
+
+	pt.Content = string(blackfriday.Run(bb[1]))
+
+	nfn := strings.TrimSuffix(p.src2dst(fp), MdExt) + ".html"
+	f, err := p.newFile(nfn)
+	if err != nil {
+		log.Printf("parsePost newFile %v: %v\n", nfn, err)
+		return pt
+	}
+	defer f.Close()
+
+	err = p.t.ExecuteTemplate(f, tmpl, pt)
+	if err != nil {
+		log.Printf("parsePost exec template %v for %v: %v", tmpl, fp, err)
+		return pt
+	}
+	return pt
+}
+
+func (p *Processor) copyFile(fn string) {
+	nfn := p.src2dst(fn)
+	f, err := p.newFile(nfn)
+	if err != nil {
+		log.Printf("copyFile newFile %v: %v\n", nfn, err)
+		return
+	}
+	fo, err := os.Open(fn)
+	if err != nil {
+		log.Printf("copyFile Open %v: %v\n", fn, err)
+		return
+	}
+	defer fo.Close()
+
+	_, err = io.Copy(f, fo)
+	if err != nil {
+		log.Printf("copyFile copy %v tp %v: %v", fn, nfn, err)
 	}
 }
 
-func (w *W) Copy(fn string) {
-	w.wg.Add(1)
-	go func() {
-		defer w.wg.Done()
-
-		s, err := os.Open(fn)
-		if err != nil {
-			log.Printf("Copy error opening %v: %v\n", fn, err)
-			return
-		}
-		defer s.Close()
-
-		d, err := create(rename(fn))
-		if err != nil {
-			log.Printf("Copy error opening %v: %v\n", rename(fn), err)
-			return
-		}
-		defer d.Close()
-
-		_, err = io.Copy(d, s)
-		if err != nil {
-			log.Printf("Copy error copy %v: %v", fn, err)
-		}
-	}()
+func (p *Processor) src2dst(f string) string {
+	return filepath.Join(Dst, strings.TrimPrefix(f, Src))
 }
 
-func (w *W) Convert(fn string) {
-	w.wg.Add(1)
-	go func() {
-		defer w.wg.Done()
-
-		b, err := ioutil.ReadFile(fn)
-		if err != nil {
-			log.Printf("Convert error reading file %v: %v\n", fn, err)
-			return
-		}
-
-		nfn := rename(fn)
-		p := convert(nfn, b)
-
-		d, err := create(nfn)
-		if err != nil {
-			log.Printf("Copy error opening %v: %v\n", nfn, err)
-			return
-		}
-		defer d.Close()
-
-		err = w.t.ExecuteTemplate(d, "post", p)
-		if err != nil {
-			log.Printf("error executing template %v for %v: %v\n", "post", nfn, err)
-			return
-		}
-
-		w.posts <- p
-	}()
-}
-
-func create(fn string) (*os.File, error) {
+func (p *Processor) newFile(fn string) (*os.File, error) {
 	err := os.MkdirAll(filepath.Dir(fn), 0744)
 	if err != nil {
-		log.Printf("create error mkdirall %v: %v\n", filepath.Dir(fn), err)
-		return nil, err
+		log.Printf("newFile mkdirall %v:%v\n", filepath.Dir(fn), err)
 	}
-
 	f, err := os.Create(fn)
 	if err != nil {
-		log.Printf("create error opening %v: %v\n", fn, err)
+		log.Printf("newFile create %v: %v\n", fn, err)
+		return nil, err
 	}
-	return f, err
-}
-
-func urlname(p string) string {
-	return filepath.Join(filepath.SplitList(strings.TrimSuffix(p, filepath.Ext(p)))[1:]...)
-}
-
-func rename(p string) string {
-	e := filepath.Ext(p)
-	p = strings.TrimSuffix(p, e) + ext[e]
-	p = dst + strings.TrimPrefix(p, src)
-	return p
-}
-
-func convert(fn string, b []byte) Post {
-	bb := bytes.SplitN(b, []byte("\n---\n"), 2)
-	if len(bb) < 2 {
-		log.Printf("convert --- not found in %v\n", fn)
-	}
-
-	title, date, desc := parseMeta(bb[0])
-
-	p := Post{
-		URL:         urlname(fn),
-		Title:       title,
-		Date:        date,
-		Description: desc,
-		Content:     string(blackfriday.Run(bb[1])),
-	}
-	return p
-}
-
-func parseMeta(b []byte) (title, date, desc string) {
-	for _, b := range bytes.Split(bytes.TrimSpace(b), []byte("\n")) {
-		bb := bytes.SplitN(b, []byte("="), 2)
-		if len(bb) < 2 {
-			log.Printf("parseMeta expected split by =\n")
-			continue
-		}
-		v := string(bytes.TrimSpace(bb[1]))
-		switch string(bytes.TrimSpace(bb[0])) {
-		case "title":
-			title = v
-		case "date":
-			date = v
-		case "description", "desc":
-			desc = v
-		}
-	}
-	return title, date, desc
+	return f, nil
 }
 
 type Post struct {
-	Title       string
-	URL         string
-	Description string
-	Date        string
-	Content     string
-}
-
-type Index struct {
-	Posts []Post
-
-	// dummy for template
-	Title       string
-	Description string
-	URL         string
+	Title   string
+	URL     string
+	Desc    string
+	Date    string
+	Content string
 }
 
 // newer first
