309366eae41ad2cab4e7e39e0cdea8472283bdcd
Sean Liao <seankhliao@gmail.com>
2019-09-23 09:05:51 +0200

    add blog index

--- blog.go
+++ blog.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"path/filepath"
+	"sort"
 	"strings"
 
 	"github.com/russross/blackfriday/v2"
@@ -14,14 +15,22 @@ import (
 
 type BlogData struct {
 	URL     string
+	Date    string
 	Title   string
+	Desc    string
 	Content string
+
+	// only for index page
+	Posts []BlogData
 }
 
 // BlogOptions holds config needed for parsing blog posts
 // expects a flat directory of files named: yyyy-mm-dd-some-titile.md
+// evenything before the first "\n---\n" is used as a description
+//
 // dependencies on:
 //      template named "blogpost"
+//      template names "blogindex"
 type BlogOptions struct {
 	Src string
 	Dst string
@@ -41,6 +50,9 @@ func (o *BlogOptions) Exec(opt *Options) error {
 	if err != nil {
 		return fmt.Errorf("BlogOptions.Exec read dir %q: %w", o.Src, err)
 	}
+
+	var datas []BlogData
+
 	for _, fi := range fis {
 		if fi.IsDir() || filepath.Ext(fi.Name()) != ".md" {
 			continue
@@ -58,11 +70,21 @@ func (o *BlogOptions) Exec(opt *Options) error {
 			log.Printf("BlogOptions.Exec read file %q: %q\n", fi.Name(), err)
 			continue
 		}
+		bb := bytes.SplitN(b, []byte("\n---\n"), 2)
+		var desc []byte
+		if len(bb) == 2 {
+			desc = bytes.TrimSpace(bb[0])
+			b = bytes.TrimSpace(bb[1])
+		}
+
 		data := BlogData{
-			URL:     filepath.Join(opt.host, "blog", fn),
+			URL:     filepath.Join(opt.host, o.Src, fn),
+			Date:    strings.Join(parts[:3], "-"),
 			Title:   strings.Join(parts[3:], " "),
+			Desc:    string(desc),
 			Content: string(blackfriday.Run(b, blackfriday.WithRenderer(blackfriday.NewHTMLRenderer(blackfriday.HTMLRendererParameters{HeadingLevelOffset: 3, Flags: blackfriday.CommonHTMLFlags})))),
 		}
+		datas = append(datas, data)
 
 		var buf bytes.Buffer
 		err = opt.T.ExecuteTemplate(&buf, "blogpage", data)
@@ -71,11 +93,34 @@ func (o *BlogOptions) Exec(opt *Options) error {
 			continue
 		}
 
-		dfn := filepath.Join(o.Dst, "blog", fn)
+		dfn := filepath.Join(o.Dst, o.Src, fn)
 		err = ioutil.WriteFile(dfn, buf.Bytes(), 0644)
 		if err != nil {
 			log.Printf("BlogOptions.Exec write %q: %w\n", dfn, err)
 		}
 	}
-	return fmt.Errorf("ErrNotImplemented")
+
+	sort.Slice(datas, func(i, j int) bool {
+		return datas[i].Date > datas[j].Date
+	})
+
+	data := BlogData{
+		URL:   filepath.Join(opt.host, o.Src),
+		Title: o.Src,
+		Posts: datas,
+	}
+
+	var buf bytes.Buffer
+	err = opt.T.ExecuteTemplate(&buf, "blogindex", data)
+	if err != nil {
+		return fmt.Errorf("BlogOptions.Exec exec template blogindex: %w", err)
+	}
+
+	dfn := filepath.Join(o.Dst, o.Src, "index.html")
+	err = ioutil.WriteFile(dfn, buf.Bytes(), 0644)
+	if err != nil {
+		return fmt.Errorf("BlogOptions.Exec write index %q: %w", dfn, err)
+	}
+
+	return nil
 }
