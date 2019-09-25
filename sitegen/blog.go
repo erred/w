d3aae41db04da65a9486b61253480c9c9dbf7111
Sean Liao <seankhliao@gmail.com>
2019-09-26 00:35:42 +0200

    fix atom uri

--- sitegen/blog.go
+++ sitegen/blog.go
@@ -148,7 +148,7 @@ func (o *BlogOptions) Exec(opt *Options) error {
 					Type: "text/html",
 				},
 			},
-			ID:        bp.URL,
+			ID:        "https://" + bp.URL,
 			Published: atom.TimeStr(bp.Date + "T00:00:00Z"),
 			Updated:   atom.TimeStr(bp.Date + "T00:00:00Z"),
 			Author:    me,
