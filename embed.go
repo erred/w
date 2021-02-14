9029c04bd886fec167fbdbd68c724dfa5292c8c4
Sean Liao <seankhliao@gmail.com>
2021-02-14 15:10:03 +0100

    txts

--- embed.go
+++ embed.go
@@ -5,10 +5,13 @@ import (
 	"text/template"
 )
 
+// S contains the static files
+//
 //go:embed public/*
 var S embed.FS
 
 //go:embed templates/*
 var t embed.FS
 
+// T contains the parsed templates
 var T = template.Must(template.Must(template.ParseFS(t, "templates/*")).ParseFS(S, "public/base.css"))
