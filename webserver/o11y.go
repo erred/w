c56ea4d81554fec232e02a1fd7d284cedfa5fa4f
Sean Liao <seankhliao@gmail.com>
2021-06-28 21:35:19 +0200

    update resource detection

--- webserver/o11y.go
+++ webserver/o11y.go
@@ -3,10 +3,13 @@ package webserver
 import (
 	"context"
 	"fmt"
+	"path"
+	"runtime/debug"
 	"time"
 
 	"go.opentelemetry.io/contrib/propagators/jaeger"
 	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric"
 	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
@@ -16,7 +19,9 @@ import (
 	controller "go.opentelemetry.io/otel/sdk/metric/controller/basic"
 	processor "go.opentelemetry.io/otel/sdk/metric/processor/basic"
 	"go.opentelemetry.io/otel/sdk/metric/selector/simple"
+	"go.opentelemetry.io/otel/sdk/resource"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 )
 
 type stopper interface {
@@ -47,6 +52,25 @@ func o11y(ctx context.Context, endpoint string) (*shutdown, error) {
 		return nil, nil
 	}
 
+	var attrs []attribute.KeyValue
+	bi, ok := debug.ReadBuildInfo()
+	if ok {
+		attrs = append(attrs, semconv.ServiceNameKey.String(path.Base(bi.Main.Path)))
+		attrs = append(attrs, semconv.ServiceVersionKey.String(bi.Main.Version))
+	}
+
+	opts := []resource.Option{
+		resource.WithAttributes(attrs...),
+		resource.WithProcessRuntimeName(),
+		resource.WithProcessRuntimeVersion(),
+		resource.WithProcessRuntimeDescription(),
+	}
+
+	res, err := resource.New(ctx, opts...)
+	if err != nil {
+		return nil, fmt.Errorf("create resource: %w", err)
+	}
+
 	// trace
 	traceExporter, err := otlptrace.New(ctx, otlptracegrpc.NewClient(
 		otlptracegrpc.WithInsecure(),
@@ -56,6 +80,7 @@ func o11y(ctx context.Context, endpoint string) (*shutdown, error) {
 		return nil, fmt.Errorf("create otlptrace exporter: %w", err)
 	}
 	traceProvider := sdktrace.NewTracerProvider(
+		sdktrace.WithResource(res),
 		sdktrace.WithBatcher(traceExporter),
 	)
 	otel.SetTracerProvider(traceProvider)
